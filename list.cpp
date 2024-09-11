be0ee8fafe9d6aec87640e3044cb0b456c2b38b0
Novation257 <91281416+Novation257@users.noreply.github.com>
2024-09-11 13:24:42 -0400

    Update list.cpp
    First iteration of list.cpp - ready to be tested

--- list.cpp
+++ list.cpp
@@ -1,11 +1,28 @@
 #include "volsort.h"
 
-
+// Constructor - Copied from Lab4 list.cpp from CS202
 List::List() {
-
-
+    head = NULL;
 }
 
+// Deconstructor - Copied from Lab4 list.cpp from CS202
 List::~List() {
+    if (head == NULL) {     // If the LL is not already empty...
+        Node *p= head;          // Retrieve first node
+        while (p != NULL) {     // While there are still nodes in the chain...
+            Node *next = p->next;   // Retrieve next linked node
+            delete p;               // Delete current node
+            p = next;               // Navigate to next node
+        }
+    }
+}
 
+// Compares the int values stored in two nodes - returns true if a > b
+bool node_number_compare(const Node *a, const Node *b) {
+    return(a->number > b->number);
+}
+
+// Compares the strings stored in two nodes - returns true if a > b
+bool node_string_compare(const Node *a, const Node *b) {
+    return(a->string > b->string);
 }
