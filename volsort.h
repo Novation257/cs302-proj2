2ce5f686d1a03323bb2f8b938e8eba5faf0417f6
Novation257 <91281416+Novation257@users.noreply.github.com>
2024-09-11 13:26:17 -0400

    Update volsort.h
    Additional changes to the List struct

--- volsort.h
+++ volsort.h
@@ -15,17 +15,25 @@ struct List {
     Node       *head;
     size_t      size;
 
-    List(); 					// define in list.cpp
-    ~List();					// define in list.cpp
+    List(); 					// define in list.cpp - COMPLETED
+    ~List();					// define in list.cpp - COMPLETED
 
-  //    void push_front(const std::string &s);	//define below
+    void push_front(const std::string &s) { 	//define below - COMPLETED
+      // Allocate a new node, copying supplied data into it.
+      Node *nn = new Node;
+      nn->string = s;
+      nn->number = stoi(s);
+
+      nn->next = head; // Set new node's pointer to the current head (will be null if LL is empty)
+      head = nn;       // Set the LL's head node to the new node (push front)
+    }
 };
 
 
 // Functions -------------------------------------------------------------------
 
-bool node_number_compare(const Node *a, const Node *b); 	//implement in list.cpp to avoid compile-time issues, used by quick, merge and stl
-bool node_string_compare(const Node *a, const Node *b);		//implement in list.cpp to avoid compile-time issues, merge and stl
+bool node_number_compare(const Node *a, const Node *b); 	//implement in list.cpp to avoid compile-time issues, used by quick, merge and stl - COMPLETED
+bool node_string_compare(const Node *a, const Node *b);		//implement in list.cpp to avoid compile-time issues, merge and stl - COMPLETED
 
 void dump_node(Node *n);					// implement in this file (volsort.h) to make it easier for TAs to grade
 
