53cf0272aa04c5d9d6e58aec5dad937627d8f700
rwolpert8 <rwolpert@vols.utk.edu>
2024-09-14 12:39:19 -0400

    Update stl.cpp

--- stl.cpp
+++ stl.cpp
@@ -4,7 +4,37 @@
 
 #include <algorithm>
 #include <iostream>
+#include <array>
 
 void stl_sort(List &l, bool numeric) {
+    // Create an array to store pointers to the nodes
+    Node** nodes = new Node*[l.size];
+    
+    // Fill the array with pointers to each node in the list
+    Node* current = l.head;
+    size_t index = 0;
+    while (current != nullptr) {
+        nodes[index++] = current;
+        current = current->next;
+    }
+    
+    // Sort the array using std::sort and the appropriate comparison function
+    if (numeric) {
+        std::sort(nodes, nodes + l.size, node_number_compare);
+    } 
+    else {
+        std::sort(nodes, nodes + l.size, node_string_compare);
+    }
+    
+    // Reconstruct the linked list by updating the next pointers
+    for (size_t i = 0; i < l.size - 1; ++i) {
+        nodes[i]->next = nodes[i + 1];
+    }
+    nodes[l.size - 1]->next = nullptr; // Last node should point to nullptr
+    
+    // Update the head of the list
+    l.head = nodes[0];
+    
+    // Clean up the allocated array
+    delete[] nodes;
 }
-
